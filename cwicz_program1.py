8a7cdf1b4d4e917e195d385236df003cf711dcac
Aga <agwe@onet.eu>
2020-08-10 21:11:39 +0200

    first commit

---
+++ cwicz_program1.py
@@ -0,0 +1,13 @@
+print("Jak masz na imię?")
+
+imie = input()
+
+print("Hej, cześć", imie)
+
+print("Ile masz lat?")
+
+wiek = int(input())
+
+wiekZaRok = str(wiek + 1)
+
+print("Za rok będziesz mieć" + wiekZaRok + "lat")
